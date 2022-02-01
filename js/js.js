fcba76832f30f514cbd2c463e3e9d1f1dd022a53
mdcrodrigo <mdcrodrigo@hotmail.com>
2022-02-01 17:57:37 -0300

    Placar

--- js/js.js
+++ js/js.js
@@ -6,10 +6,14 @@ function start() { // Inicio da função start()
 	$("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
 	$("#fundoGame").append("<div id='inimigo2'></div>");
 	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
+    $("#fundoGame").append("<div id='placar'></div>");
 
     //Principais variáveis do jogo
 	
 	var jogo = {};
+    var pontos=0;
+    var salvos=0;
+    var perdidos=0;
     var fimdejogo=false;
     var podeAtirar=true;
     var velocidade=5;
@@ -45,6 +49,7 @@ function start() { // Inicio da função start()
     moveinimigo2();
     moveamigo();
     colisao();
+    placar();
 
 	} 
     // Fim da função loop()
@@ -204,7 +209,8 @@ function start() { // Inicio da função start()
 		
 	    if (colisao3.length>0) {
 		
-		
+        pontos=pontos+100;
+
         inimigo1X = parseInt($("#inimigo1").css("left"));
         inimigo1Y = parseInt($("#inimigo1").css("top"));
             
@@ -221,6 +227,8 @@ function start() { // Inicio da função start()
 		
 	    if (colisao4.length>0) {
 		
+        pontos=pontos+50;
+
         inimigo2X = parseInt($("#inimigo2").css("left"));
         inimigo2Y = parseInt($("#inimigo2").css("top"));
         $("#inimigo2").remove();
@@ -236,6 +244,8 @@ function start() { // Inicio da função start()
 		
 	    if (colisao5.length>0) {
 		
+        salvos++;
+
         reposicionaAmigo();
         $("#amigo").remove();
         }
@@ -244,6 +254,8 @@ function start() { // Inicio da função start()
 		
         if (colisao6.length>0) {
 	    
+        perdidos++;
+
         amigoX = parseInt($("#amigo").css("left"));
         amigoY = parseInt($("#amigo").css("top"));
         explosao3(amigoX,amigoY);
@@ -356,5 +368,13 @@ function start() { // Inicio da função start()
         }
         
         } // Fim da função explosao3
+
+        
+        function placar() {
+	
+            $("#placar").html("<h2> Pontos: " + pontos + " Salvos: " + salvos + " Perdidos: " + perdidos + "</h2>");
+            
+        } //fim da função placar()
+        
             
 }// Fim da função start
