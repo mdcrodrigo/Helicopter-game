921451532cb026382e65701893d3c46c321ebc4e
mdcrodrigo <mdcrodrigo@hotmail.com>
2022-02-01 18:08:48 -0300

    Barra de energia

--- js/js.js
+++ js/js.js
@@ -7,10 +7,12 @@ function start() { // Inicio da função start()
 	$("#fundoGame").append("<div id='inimigo2'></div>");
 	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
     $("#fundoGame").append("<div id='placar'></div>");
+    $("#fundoGame").append("<div id='energia'></div>");
 
     //Principais variáveis do jogo
 	
 	var jogo = {};
+    var energiaAtual=3;
     var pontos=0;
     var salvos=0;
     var perdidos=0;
@@ -50,6 +52,7 @@ function start() { // Inicio da função start()
     moveamigo();
     colisao();
     placar();
+    energia();
 
 	} 
     // Fim da função loop()
@@ -183,6 +186,8 @@ function start() { // Inicio da função start()
             
         if (colisao1.length>0) {
                 
+        energiaAtual--;
+
         inimigo1X = parseInt($("#inimigo1").css("left"));
         inimigo1Y = parseInt($("#inimigo1").css("top"));
         explosao1(inimigo1X,inimigo1Y);
@@ -369,12 +374,41 @@ function start() { // Inicio da função start()
         
         } // Fim da função explosao3
 
-        
+
         function placar() {
 	
             $("#placar").html("<h2> Pontos: " + pontos + " Salvos: " + salvos + " Perdidos: " + perdidos + "</h2>");
             
         } //fim da função placar()
+
+        //Barra de energia
+
+function energia() {
+	
+    if (energiaAtual==3) {
+        
+        $("#energia").css("background-image", "url(imgs/energia3.png)");
+    }
+
+    if (energiaAtual==2) {
+        
+        $("#energia").css("background-image", "url(imgs/energia2.png)");
+    }
+
+    if (energiaAtual==1) {
+        
+        $("#energia").css("background-image", "url(imgs/energia1.png)");
+    }
+
+    if (energiaAtual==0) {
+        
+        $("#energia").css("background-image", "url(imgs/energia0.png)");
+        
+        //Game Over
+    }
+
+} // Fim da função energia()
+
         
             
 }// Fim da função start
